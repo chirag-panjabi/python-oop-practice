3d169c3c4c45810d2bdb544c2cdb674dbbc5c973
chirag-panjabi <panjabichirag1@gmail.com>
2026-03-13 02:20:14 +0530

    initial commit + 1st class example

---
+++ basics/01_classes.py
@@ -0,0 +1,11 @@
+class Dog:
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+    def bark(self):
+        return f'{self.name} says: WOOF!'
+
+dog = Dog('bruno', 'pug')
+
+
+print(dog.bark())
